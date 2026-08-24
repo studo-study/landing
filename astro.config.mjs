fb97f70992d4871d0ae5cf61cae56978b6738bfc
Charles <charles.degraeuwe@icloud.com>
2026-08-24 12:42:36 +0200

    feat: footer (#3)
    * feat: footer
    
    * fix: hero fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-
-import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: 'https://studo.study',
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
   }
--- src/components/icons/IconProps.ts
+++
@@ -1,4 +0,0 @@
-import type { SVGProps } from "react";
-export type IconProps = SVGProps<SVGSVGElement> & {
-    size?: number | string;
-};
